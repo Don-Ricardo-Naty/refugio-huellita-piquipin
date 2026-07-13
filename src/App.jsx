6e22d818391715d4f49cb4cab3b40f91ae24dc3d
Don-Ricardo-Naty <ibankavera@gmail.com>
2026-07-13 16:28:33 -0400

    cambios 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-/////////////////////////////////////////////////////////////////////////////////////
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; 
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Inicio from './components/Inicio';
 import Acerca from './components/Acerca';
 import Formulario from './components/Formulario';
@@ -8,14 +7,13 @@ import Footer from './components/Footer';
 function App() {
   return (
     <Router>
-      {/* "minHeight: '100vh'" como estilo para que tire el footer abajo*/}
       <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', fontFamily: 'Segoe UI, Tahoma, Geneva, Verdana, sans-serif', margin: 0 }}>
 
-        {/*Barra de arriba*/}
+        {/* Barra de arriba */}
         <header style={{ backgroundColor: '#4c0e2d', padding: '15px 20px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
           <div style={{ color: '#e7dee2', fontWeight: 'bold', fontSize: '1.5rem', display: 'flex', alignItems: 'center', gap: '8px' }}>
             🐾 Huellita Piquipin
-          </div>/////////////////////////////////////////////
+          </div>
           <nav style={{ display: 'flex', gap: '20px' }}>
             <Link to="/" style={{ color: 'white', textDecoration: 'none', fontWeight: 'bold' }}>Inicio</Link>
             <Link to="/ser-voluntario" style={{ color: 'white', textDecoration: 'none', fontWeight: 'bold' }}>Ser Voluntario</Link>
@@ -23,17 +21,19 @@ function App() {
           </nav>
         </header>
 
-        {/*vista principal*/}
+        {/* Vista principal */}
         <main style={{ flex: '1', backgroundColor: '#fff' }}>
-          <Routes>//////////////////////////////////////////////////////////////////////////
+          <Routes>
             <Route path="/" element={<Inicio />} />
             <Route path="/ser-voluntario" element={<Formulario />} />
             <Route path="/nosotros" element={<Acerca />} />
+            {/* Ruta comodín para redirigir si el usuario recarga la página en internet */}
+            <Route path="*" element={<Inicio />} />
           </Routes>
         </main>
 
-        {/*footer*/}
-        <Footer /> {/* uno cuando baja lo ve los datos de contacto y asi*/}
+        {/* Footer */}
+        <Footer />
 
       </div>
     </Router>
